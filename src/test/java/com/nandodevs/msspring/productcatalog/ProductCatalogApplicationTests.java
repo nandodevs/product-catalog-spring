e0dc01b82022358b5d8695aee4741ac6b19a896c
unknown <nando.devs@gmail.com>
2021-10-31 21:23:29 -0300

    first commit

---
+++ src/test/java/com/nandodevs/msspring/productcatalog/ProductCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nandodevs.msspring.productcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
